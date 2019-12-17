837a50908c37113a5fc830b6f39ba55744f406ce
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-12-17 09:25:20 -0600

    Capitulo 18 terminado

--- GestionPedidos/models.py
+++ GestionPedidos/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class Clientes(models.Model):
 
     nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50, verbose_name="La dirección")
     email = models.EmailField(blank=True, null=True)
     tfno = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.nombre
 
 class Articulos(models.Model):
 
